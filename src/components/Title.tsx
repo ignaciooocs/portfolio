f3d843c5570b3380362bdb5c1ba17f30327f0b66
ignaciooocs <naxomc25@gmail.com>
2023-12-05 22:51:45 -0300

    Fixed the error in the header image.

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,7 +4,7 @@ export default function Title() {
   return (
     <section className="flex gap-3 title items-center">
       <figure className="rounded-full shadow-md">
-        <Image src='/developer (1).png' alt="profile" width={25} height={25} className="rounded-full" priority />
+        <Image src='/developer.png' alt="profile" width={25} height={25} className="rounded-full" priority />
       </figure>
       <h1 className='font-bold text-slate-900 dark:text-white'>Ignacio Mejías Cisternas</h1>
     </section>
